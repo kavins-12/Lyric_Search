c1f220315a0e0dcea53799282d51856485a5f7f7
kavins-12 <kavins1012@gmail.com>
2020-08-06 23:09:27 +0530

    Updates

---
+++ precache-manifest.6f0d378053551668e6ae553e77669fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08a259d5ff0ed99a9e53d7ad337573a",
+    "url": "/Lyric_Search/index.html"
+  },
+  {
+    "revision": "09b80af813d8219e7370",
+    "url": "/Lyric_Search/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b454731622084457fec7",
+    "url": "/Lyric_Search/static/js/2.8e15451a.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/Lyric_Search/static/js/2.8e15451a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b80af813d8219e7370",
+    "url": "/Lyric_Search/static/js/main.7384307a.chunk.js"
+  },
+  {
+    "revision": "974a9822853ae531b381",
+    "url": "/Lyric_Search/static/js/runtime-main.0cf8e223.js"
+  },
+  {
+    "revision": "9463db668b2bb489f76de306eef7dc35",
+    "url": "/Lyric_Search/static/media/spinner.9463db66.gif"
+  }
+]);
