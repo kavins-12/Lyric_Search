a4547f84aba98f4a6fde32b785843f4c54d90fab
kavins-12 <kavins1012@gmail.com>
2020-08-06 23:06:01 +0530

    Lyric Search

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar= () =>{
+    return(
+        <nav className="navbar navbar-dark bg-dark mb-5 ">
+            <span className="navbar-brand h1 mb-0 mx-auto"> Lyric finder</span>
+
+        </nav>
+    )
+}
+
+export default Navbar;
